e61627293e2618cd4e9323b38ce903fbc6579f4d
Carolina Acosta <caro.acosta123@gmail.com>
2024-07-27 12:17:10 -0300

    New values for test

--- mainSerial.cpp
+++ mainSerial.cpp
@@ -189,10 +189,10 @@ int main() {
         auto start = std::chrono::system_clock::now();
 
         // PARAMETROS
-        int truckQty = 2; // Numero de camiones
+        int truckQty = 3; // Numero de camiones
         std::string selectedDate = "2018-08-11";
         int maxLevel = 2; // Nivel hasta el que se expande con BFS
-        int maxStops = 12; // Total 654
+        int maxStops = 11; // Total 654
 
         // INICIALIZACION DE VARIABLES Y EXTRACCION DE DATOS
         std::unordered_map<std::string, Stop> stopsHash;
@@ -217,7 +217,7 @@ int main() {
             Truck truckToSave;
             truckToSave.id = truckId;
             // truckToSave.capacity = getNthValue(truckCapacities, rnd);
-            truckToSave.capacity = 415000;
+            truckToSave.capacity = 270000;
             truckHash[truckId] = truckToSave;
         };
 
